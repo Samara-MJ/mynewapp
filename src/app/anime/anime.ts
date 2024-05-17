3ab750696c0e11407a5250a87394327cf0e1f521
Samara Martínez <s.martinezj@uniandes.edu.co>
2024-05-17 11:22:40 -0500

    :((((

--- src/app/anime/anime.ts
+++ src/app/anime/anime.ts
@@ -1,24 +1,35 @@
 export class Anime {
         id: number;
         name: string;
-        categoria: string;
-        calificacion: string;
-        image: string;
+        description: string;
+        Rating: string;
+        episode: string;
+        categorie: string;
+        studio: string;
+        img: string;
         
        
         constructor(
-            id: number,
-            name: string,
-            categoria: string,
-            calificacion: string,
-            image: string,
+          id: number,
+          name: string,
+          description: string,
+          Rating: string,
+          episode: string,
+          categorie: string,
+          studio: string,
+          img: string
             
         ) {
         this.id = id;
+        this.id = id;
           this.name = name;
-          this.categoria = categoria;
-          this.calificacion = calificacion;
-          this.image = image;
+          this.categorie = categorie;
+          this.description = description;
+          this.Rating = Rating;
+          this.episode = episode;
+          this.studio = studio;
+          this.img = img;
+          
         
        }
 }
